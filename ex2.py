4bdabf9d7f1bf6f7a32d189d6789f929850b9651
muravevas <muravevartem@outlook.com>
2022-10-18 08:59:00 +0400

    fix

--- ex2.py
+++ ex2.py
@@ -1,18 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import logging
-
-from prettytable import PrettyTable
 
 AMOUNT_OF_RAND_NUMBER = 100
 NUMBER_OF_INTERVALS = 8
-TITLE_BAR = 'ex2_bar'
-TITLE_GRAPH_F = 'F(x)'
-TITLE_GRAPH_FX = 'F*(x)'
 
 
 def generator(amount_of_rand_number):
-    return np.random.rand(amount_of_rand_number) ** (2/3)
+    return [(1 - (1 - u**2)**0.5)/u for u in np.random.rand(amount_of_rand_number)]
 
 
 def getIndexInterval(x, min_x, max_x, h):
@@ -61,52 +55,10 @@ def f(x):
     return x * x ** 0.5
 
 
-def drawBar(min_x, max_x, h, amount_of_rand_numbers, n_array):
-    y = [n / (amount_of_rand_numbers) * h for n in n_array]
-    x = [min_x + h * i for i in range(len(n_array))]
-    plt.title(TITLE_BAR)
-    plt.grid(True)
-    plt.xlabel('x')
-    plt.ylabel('nₗ / np')
-    plt.bar(x, y, width=h, color='lavender', edgecolor='black', align='edge')
-    plt.xticks(np.arange(0, 1.1, 0.1))
-    #x = np.arange(0,1,0.01)
-    #y = [3/2*x**0.5 for x in x]
-    #plt.plot(x,y)
-    plt.savefig("ex2_bar.svg")
-    plt.show()
-
-
-def drawGraph(min_x, max_x, h, n_array):
-    plt.title(TITLE_GRAPH_F)
-    plt.grid(True)
-    x_array = np.arange(-1, 2, 0.001)
-    plt.plot(x_array, [f(x) for x in x_array])
-    plt.ylabel('y')
-    plt.xlabel('x')
-    plt.savefig("ex2_graph_f(x).svg")
-    plt.show()
-    plt.clf()
-    plt.title(TITLE_GRAPH_FX)
-    plt.grid(True)
-    plt.plot(x_array, [fx(x, min_x, max_x, h, n_array) for x in x_array])
-    plt.ylabel('y')
-    plt.xlabel('x')
-    plt.savefig("ex2_graph_fx(x).svg")
-    plt.show()
-
-
 def test(min_x, max_x, h, n_array):
     d_array = [f(min_x + h * x) - fx(min_x + h * x, min_x, max_x, h, n_array) for x in range(len(n_array))]
     d_n = max(d_array)
     return d_n, d_n * 10
-
-
-def table(min_x, h, number_of_intervals, n_array):
-    table = PrettyTable(["a[i-1]", "a[i]", "n[i]"])
-    for i in range(number_of_intervals):
-        table.add_row([min_x + h * i, min_x + h * (i + 1), n_array[i]])
-    print(table)
 
 
 def main():
@@ -119,9 +71,6 @@ def main():
     logging.info(f"Хв: {x_v} Дв: {d_v} δ: {sqrt_d_v}")
     d_n, lambda_b = test(min_x, max_x, h, n_array)
     logging.info(f"Dn: {d_n} λв: {lambda_b}")
-    drawBar(min_x, max_x, h, AMOUNT_OF_RAND_NUMBER, n_array)
-    drawGraph(min_x, max_x, h, n_array)
-    table(min_x, h, NUMBER_OF_INTERVALS, n_array)
 
 
 main()
